853d52ff355a6de5c54365dadf0bacc727ef1816
Corentin SCIALPI <corentin.scialpi@gmail.com>
2023-05-15 15:08:49 +0200

    Update test_game.py

--- gui/tests/unit_tests/test_game.py
+++ gui/tests/unit_tests/test_game.py
@@ -12,6 +12,6 @@ class TestGame:
     def test_construct_game_works(self) -> None:
         """Test if the constructor of Game works"""
         game = Game()
-        assert game.running == True
+        assert game.running is True
         assert isinstance(game.screen, pygame.Surface)
         assert isinstance(game.windows_size, Coordinate)
